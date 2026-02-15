e933aeb332c278431296c54343b8978234bfad42
Faisal Alsayyari <sayyarifaisal@gmail.com>
2026-02-15 15:52:43 +0000

    Implemented POSIX-based timing logic and sampling.

--- core/osl.h
+++ core/osl.h
@@ -8,18 +8,28 @@ typedef void (*osl_func)(void *);
 
 typedef uint64_t (*osl_progress)(void *);
 
+typedef void *(*osl_create_ctx)(void);
+
+typedef void (*osl_destroy_ctx)(void *);
+
 typedef struct {
 	const char *name;
 	osl_func func;
 } osl_impl;
 
+// TODO optimize this for struct padding :)
+// it shouldn't be awkward for the user since they (hopefully)
+// will write the create_ctx() function so member creation order
+// won't matter
+
 typedef struct {
 	void *ctx;
-	osl_progress progress;
+    osl_create_ctx create_ctx; 
+    osl_destroy_ctx destroy_ctx;
+    osl_progress progress;
 	osl_impl *impls;
 	size_t impl_count;
 	size_t target_sample_count;
-	const char *output_prefix;
 } osl_benchmark;
 
 #define OSL_REGISTER(name, fn) {#name, fn }
--- core/timing.c
+++ core/timing.c
@@ -1,76 +1,96 @@
+#define _POSIX_C_SOURCE 200809L
 #include <math.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 #include <pthread.h>
 
-#include "registration.h"
+#include "osl.h"
 
-#define MAX_TIME (600)
-#define MAX_STRING_SIZE (256)
+#define MAX_TIME (30)
+#define FILENAME_LENGTH (256)
+#define FPS (30)
 
-void osl_run(osl_benchmark *benchmark);
+void osl_run(osl_benchmark *benchmark, double seconds);
 void osl_run_individual(osl_benchmark *benchmark, osl_impl *implementation, double seconds);
 
 void osl_run(osl_benchmark *benchmark, double seconds) {
-	for (int i = 0; i < benchmark->impl_count; i++) {
-		osl_run_individual(benchmark, benchmark->impls[i], seconds);
-	
+   
+	if (seconds >= MAX_TIME) {
+		printf("ERROR: for your sanity, the current maximum benchmark time supported is 30 seconds.\n");
 	}
+
+    printf("--- Benchmark Started ---");
+
+   // This forces all .dat files to target 30 FPS.
+   // In future versions we may allow the user to define FPS
+
+   benchmark->target_sample_count = FPS * seconds; 
+
+    for (size_t i = 0; i < benchmark->impl_count; i++) {
+		void *ctx = benchmark->create_ctx();
+        benchmark->ctx = ctx;
+        osl_run_individual(benchmark, &benchmark->impls[i], seconds);
+	    benchmark->destroy_ctx(benchmark->ctx);
+
+	}
+
+    printf("--- Benchmark Completed ---\n");
+
 }
 
 void osl_run_individual(osl_benchmark *benchmark, osl_impl *implementation, double seconds) {
 
-	if (seconds >= MAX_TIME) {
-		printf("That's a LONG time... you might wanna reconsider :P\n");
-	}
+    char filename[FILENAME_LENGTH];
 
-	// Construct the filename first
-
-	// TODO this might not be doing what I think it's doing...
-	// It might be appending the prefix to 256 garbage chars :)	
-
-	char *filename = malloc(sizeof(char) * MAX_STRING_SIZE);
-	strncpy(filename, benchmark->output_prefix, MAX_STRING_SIZE);
-	strncat(filename, implementation->name, MAX_STRING_SIZE);
-	strncat(filename, ".dat", MAX_STRING_SIZE);
+    if (snprintf(filename, FILENAME_LENGTH, "osl_%s.dat", implementation->name) < 0) {
+        printf("Filename encoding error on %s :(\n", implementation->name);
+        return;
+    }
 
 	FILE *file = fopen(filename, "w");
 
 	if (file == NULL) {
 		printf("Error opening file on implementation %s\n", implementation->name);
-		return 1;
+		return;
 	}
 
 
 	struct timespec start, now;
 	clock_gettime(CLOCK_MONOTONIC, &start);
+
+    // sidenote: elapsed is in units of seconds (s)
+    //           time_since_last write and target_write interval are
+    //           in units of nanoseconds (ns)
+
+    int datapoints_collected = 0;
+    double elapsed = 0;
+    double time_since_last_write = 0;
+    double target_write_interval = (seconds / benchmark->target_sample_count) * 1e9; 
+    while (elapsed < seconds) {
 	
-	char *output_buffer = malloc(sizeof(char) * );
+		implementation->func(benchmark->ctx);
 
-	// TODO iterations will be redundant once we get proper sampling logic based on benchmark->target_sample_rate done...
-	int iterations = 0;
-	while (1) {
-	
-		implementation->func();
-		iterations++;
+        clock_gettime(CLOCK_MONOTONIC, &now);
+	    double elapsed = (now.tv_sec - start.tv_sec) +
+				         (now.tv_nsec - start.tv_nsec) / 1e9;
+        time_since_last_write += elapsed;
 		
-		if ((iterations % benchmark->sample_rate) == 0) {
-			clock_gettime(CLOCK_MONOTONIC, &now);
-			double elapsed = (now.tv_sec - start.tv_sec) +
-				 (now.tv_nsec - start.tv_nsec) / 1e9;
+        if (time_since_last_write >= target_write_interval) {
 
-			if (elapsed >= seconds) break;
-
-			// file writing logic
-		
+			// file writing
 			uint64_t progress = benchmark->progress(benchmark->ctx);
-	        fprintf(file, "%llu :: %.9f\n", progress, elapsed);	
+	        fprintf(file, "%lu :: %.9f\n", progress, elapsed);	
 
+            time_since_last_write = 0;
+            datapoints_collected++;
 		}
 	}
 
+    printf("Implementation %s successfully wrote %d datapoints to %s\n", implementation->name, datapoints_collected, filename);
+
 	fclose(file);
-	free(filename);
+    file = NULL;
 
 }
